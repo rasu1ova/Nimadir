6de5374a039be82e4644352025754b06c38e6a8d
rasu1ova <108572601+rasu1ova@users.noreply.github.com>
2023-05-17 17:12:57 +0500

    almost finished the aside part

--- src/components/Allegria/Allegria.jsx
+++ src/components/Allegria/Allegria.jsx
@@ -8,12 +8,14 @@ function Allegria() {
   const [colors, setColors] = useState([]);
   const [material, setMaterial] = useState([]);
   const [currMaterial, setCurrMaterial] = useState("");
+  const [currColors, setCurrColors] = useState("");
   const [currSize, setCurrSize] = useState([]);
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(200);
 
   const colorArray = new Set();
   const materialArray = new Set();
   useEffect(() => {
-    // setLoading(true);
     fetch("https://my-json-server.typicode.com/rasu1ova/Server/clothes")
       .then((response) => response.json())
       .then((data) => {
@@ -22,35 +24,58 @@ function Allegria() {
       })
       .catch((error) => console.log(error));
   }, []);
+  const handleAllButtonClick = () => {
+    setFilterData(clothes);
+  };
   useEffect(() => {
     clothes.map((option) => {
       colorArray.add(option.color);
       materialArray.add(option.material);
       setColors([...colorArray]);
+
       setMaterial([...materialArray]);
     });
   }, [clothes]);
+
+  useEffect(() => {
+    const filterColors = currColors
+      ? clothes.filter((dataColor) => dataColor.color === currColors)
+      : clothes;
+    setFilterData(filterColors);
+  }, [currColors]);
+
   useEffect(() => {
     const filterMaterial = currMaterial
       ? clothes.filter((dataMaterial) => dataMaterial.material === currMaterial)
       : clothes;
-   
     setFilterData(filterMaterial);
-
   }, [currMaterial]);
 
   useEffect(() => {
-    const filterSize = currSize
-    ? filterData.filter((dataSize) => {
-        return currSize.map((items) => {
-          console.log(dataSize.size.includes(items));
-          return dataSize.size.includes(items);
-        });
-      })
-    : filterData;
+    const filterSize =
+      currSize.length >= 1
+        ? clothes.filter((item) =>
+            item.size.some((size) => currSize.includes(size))
+          )
+        : clothes;
     setFilterData(filterSize);
   }, [currSize]);
-  console.log(filterData);
+  useEffect(() => {
+    console.log(fromPrice);
+    console.log(toPrice);
+    console.log(fromPrice < toPrice);
+    console.log(typeof fromPrice);
+    console.log(typeof toPrice);
+    const filterPrice =
+      fromPrice < toPrice
+        ? clothes.filter(
+            (item) => fromPrice < item.price && item.price < toPrice
+          )
+        : clothes;
+    console.log(filterPrice);
+    setFilterData(filterPrice);
+  }, [fromPrice, toPrice]);
+
   const handleSizeChange = (e) => {
     const { value } = e.target;
     if (currSize.includes(value)) {
@@ -62,11 +87,107 @@ function Allegria() {
 
   return (
     <div className="container home">
+      <div className="product-list">
+        {filterData &&
+          filterData.map((item) => (
+            <Card
+              key={item.id}
+              name={item.name}
+              price={item.price}
+              image={item.img}
+              color={item.color}
+              material={item.material}
+            />
+          ))}
+      </div>
       <aside>
-        <div className="">
+        <div className="col">
+          <button onClick={handleAllButtonClick}>All</button>
+        </div>
+        <div className="col">
+          <h2>Size:</h2>
+          <div className="row--start">
+            <label className="checkboxes">
+              <input
+                type="checkbox"
+                name="checkbox"
+                value="S"
+                id="checkboxes"
+                onChange={handleSizeChange}
+                checked={currSize.includes("S")}
+              />
+              S
+            </label>
+            <label className="checkboxes">
+              <input
+                type="checkbox"
+                name="checkbox"
+                value="M"
+                id="checkboxes"
+                onChange={handleSizeChange}
+                checked={currSize.includes("M")}
+              />
+              M
+            </label>
+          </div>
+          <div className="row--start">
+            <label className="checkboxes">
+              <input
+                type="checkbox"
+                name="checkbox"
+                value="L"
+                id="checkboxes"
+                onChange={handleSizeChange}
+                checked={currSize.includes("L")}
+              />
+              L
+            </label>
+            <label className="checkboxes">
+              <input
+                type="checkbox"
+                name="checkbox"
+                value="XL"
+                id="checkboxes"
+                onChange={handleSizeChange}
+                checked={currSize.includes("XL")}
+              />
+              XL
+            </label>
+          </div>
+        </div>
+        <div className="row--start">
+          <div className="col">
+            From
+            <input
+              type="number"
+              name=""
+              className="price"
+              value={fromPrice}
+              onChange={(e) => {
+                setFromPrice(Number(e.target.value));
+              }}
+            />
+          </div>
+          <div className="col">
+            To
+            <input
+              type="number"
+              name=""
+              className="price"
+              value={toPrice}
+              onChange={(e) => {
+                setToPrice(Number(e.target.value));
+              }}
+            />
+          </div>
+        </div>
+        <div className="row--start">
           <div className="col">
             <h2>Color:</h2>
-            <select>
+            <select
+              onChange={(e) => {
+                setCurrColors(e.target.value);
+              }}>
               {colors &&
                 colors.map((items) => {
                   return (
@@ -93,80 +214,8 @@ function Allegria() {
                 })}
             </select>
           </div>
-          <div className="col">
-            <h2>Size:</h2>
-            <label className="checkboxes">
-              <input
-                type="checkbox"
-                name="checkbox"
-                value="X"
-                id="checkboxes"
-                onChange={handleSizeChange}
-                checked={currSize.includes("X")}
-              />
-              X
-            </label>
-            <label className="checkboxes">
-              <input
-                type="checkbox"
-                name="checkbox"
-                value="S"
-                id="checkboxes"
-                onChange={handleSizeChange}
-                checked={currSize.includes("S")}
-              />
-              S
-            </label>
-            <label className="checkboxes">
-              <input
-                type="checkbox"
-                name="checkbox"
-                value="M"
-                id="checkboxes"
-                onChange={handleSizeChange}
-                checked={currSize.includes("M")}
-              />
-              M
-            </label>
-            <label className="checkboxes">
-              <input
-                type="checkbox"
-                name="checkbox"
-                value="L"
-                id="checkboxes"
-                onChange={handleSizeChange}
-                checked={currSize.includes("L")}
-              />
-              L
-            </label>
-            <label className="checkboxes">
-              <input
-                type="checkbox"
-                name="checkbox"
-                value="XL"
-                id="checkboxes"
-                onChange={handleSizeChange}
-                checked={currSize.includes("XL")}
-              />
-              XL
-            </label>
-          </div>
         </div>
       </aside>
-
-      <div className="mebel-list">
-        {filterData &&
-          filterData.map((item) => (
-            <Card
-              key={item.id}
-              name={item.name}
-              price={item.price}
-              image={item.img}
-              color={item.color}
-              material={item.material}
-            />
-          ))}
-      </div>
     </div>
   );
 }
